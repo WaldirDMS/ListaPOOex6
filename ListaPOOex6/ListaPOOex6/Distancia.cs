966283b44f255de0adfc837eccfa39434579e397
WaldirDMS <75144213+WaldirDMS@users.noreply.github.com>
2020-11-27 19:02:15 -0300

    Add files via upload

---
+++ ListaPOOex6/ListaPOOex6/Distancia.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListaPOOex6
+{
+    class Distancia
+    {
+        private double milhas;
+        private double quilometros;
+
+        public void setMilhas(int p)
+        {
+            milhas = p;
+        }
+
+        public double getMilhas()
+        {
+            return milhas;
+        }
+
+        public double getQuilometros()
+        {
+            return quilometros;
+        }
+
+        public void converterDistancia()
+        {
+            quilometros = milhas * 1852;
+        }
+    }
+}
---
+++ ListaPOOex6/ListaPOOex6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ListaPOOex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Distancia dist;
+
+            dist = new Distancia();
+
+            Console.Write("Informe a distancia em milhas ");
+            dist.setMilhas(int.Parse(Console.ReadLine()));
+
+            dist.converterDistancia();
+
+            Console.WriteLine("O valor {0} em milhas é correspondente a {1} em quilometros",dist.getMilhas() ,dist.getQuilometros());
+        }
+    }
+}
